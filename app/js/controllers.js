489260718da7120bc60a7728126317bb89135a66
Derik Evangelista <kirederik@gmail.com>
2013-04-08 16:16:46 -0300

    Added more checks
    Signed-off-by: Derik Evangelista <kirederik@gmail.com>

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -12,7 +12,7 @@ function DerivativeCtrl($scope) {
 	$scope.problem = new ProblemFunction()
 	$scope.ans = {};
 	$scope.errorCount = 0;
-	$scope.flash = { 
+	$scope.flash = {
 		dx: {
 			correct: false,
 			message: "",
@@ -22,8 +22,24 @@ function DerivativeCtrl($scope) {
 			correct: false,
 			message: "",
 			error: false
+		},
+		fx: {
+			correct: false,
+			message: "",
+			error: false
 		}
 	};
+
+	$scope.refresh = function(element) {
+		console.log(element)
+		window.setTimeout( function() {
+			var math = MathJax.Hub.getAllJax(element);
+			$(math).each(function(index, value) {
+				value.Reprocess();
+			});
+		}, 100);
+	};
+
 	$scope.verifyExponencialDx = function() {
 		var ans = $scope.ans.derivative.replace(/ /g, "");
 		$scope.flash.dx.error = false;
@@ -44,13 +60,11 @@ function DerivativeCtrl($scope) {
 			$scope.errorCount++;
 		} else if (ans.indexOf("*") < 0) {
 			$scope.flash.dx.message = "Are you using * to multiplication?";
-		} else if (ans.indexOf("^") < 0) {
-			$scope.flash.dx.message = "Are you using ^ to represent power?";
 		} else if ($scope.flash.dx.error) {
-			$scope.flash.dx.message = "That's not correct! Try again!";			
+			$scope.flash.dx.message = "That's not correct! Try again!";
 			$scope.errorCount++;
 		} else {
-			$scope.errorCount = 0;			
+			$scope.errorCount = 0;
 		}
 		return false;
 	};
@@ -59,7 +73,21 @@ function DerivativeCtrl($scope) {
 		if ($scope.problem.r.name == "exponencial") {
 			$scope.verifyExponencialDx();
 		};
+		$scope.refresh("correctDerivative");
+		$scope.refresh("errorDerivative");
 	};
+
+	$scope.verifyFx = function() {
+		var answer = eval($scope.ans.fx.replace(/\,/g, "."));
+		var value = eval($scope.problem.f.eval.replace(/x/g, $scope.problem.f.x));
+		if (answer == value) {
+			$scope.flash.fx.error = false;
+		} else {
+			$scope.flash.fx.message = "That's not correct!";
+			$scope.flash.fx.error = true;
+		}
+		$scope.flash.fx.correct = !$scope.flash.fx.error;
+	}
 
 	$scope.verifyNearest = function() {
 		var answer = $scope.ans.nearest.replace(/\,/g, ".");
@@ -78,6 +106,7 @@ function DerivativeCtrl($scope) {
 		} else if (answer == nearest) {
 			$scope.flash.nearest.message = "That's correct!";
 			$scope.flash.nearest.error = false;
+			$scope.refresh("correctNearest");
 		} else {
 			$scope.flash.nearest.message = "I do not understant your answer. Please try again";
 			$scope.flash.nearest.error = true;
@@ -88,11 +117,13 @@ function DerivativeCtrl($scope) {
 
 var FunctionRule = function(rule) {
 	this.generators = [
-		{	
+		{
 			name: "exponencial",
 			rule: "x ^ n",
+			eval: "Math.pow(x, n)",
 			derivative: {
 				rule: "n*x^(n-1)",
+				eval: "n*Math.pow(x, n-1)",
 				tex: "n \\times x ^ {(n - 1)}",
 				equivalents: []
 			},
@@ -131,11 +162,15 @@ ProblemFunction.prototype = {
 	}
 	, expandFunction: function(val) {
 		this.f.fn = this.r.rule.replace(this.regex, val+2);
+		this.f.eval = this.r.eval.replace(this.regex, val+2);
+		this.f.fx = eval(this.f.eval.replace(/x/g, this.f.x));
+		this.derivative.eval = this.r.derivative.eval.replace(this.regex, val+2);
 		this.r.number = val + 2;
 		console.log("expanding rule: ", this.r.rule, " to ", this.f.fn);
 	}
 	, expandDerivative: function() {
 		this.derivative.rule = this.r.derivative.rule.replace(this.regex, this.r.number);
+		this.derivative.value = eval(this.derivative.eval.replace(/x/g, this.f.x));
 		this.derivative.tex = this.r.derivative.tex.replace(this.regex, this.r.number);
 		if (this.r.name == "exponencial") {
 			this.derivative.equivalents[0] = this.evalExpo();
@@ -153,8 +188,8 @@ ProblemFunction.prototype = {
 	, generate: function() {
 		this.r = new FunctionRule(this.randomize(0));
 		console.log("rule selected: ", this.r);
-		this.applyRule();
 		this.setValues();
+		this.applyRule();
 		console.log(this);
 		return this;
 	}
@@ -231,7 +266,7 @@ Expr.prototype = {
 		// 		console.log(expanded);
 		// 		expression = this.merge(expression, expanded, index);
 		// 		splitted = currentRule.split(" ");
-		// 		index = splitted.indexOf("EXPR");	
+		// 		index = splitted.indexOf("EXPR");
 		// 	}
 		// 	seed = this.randomize(this.rulesAmount)
 		// 	currentRule = this.rules["R"+seed];
@@ -252,7 +287,7 @@ Expr.prototype = {
 				if (i >= max) {
 					var exp = "NUMB"
 				} else {
-					var exp = this.expand(i)					
+					var exp = this.expand(i)
 				}
 				expanded = this.merge(expanded, exp, index)
 				index = expanded.indexOf("EXPR");
@@ -260,7 +295,7 @@ Expr.prototype = {
 		}
 		// console.log(expanded);
 		return expanded;
-	} 
+	}
 	, generate: function() {
 		var seed = this.randomize(this.rulesAmount-3);
 		var currentRule = this.rules["R"+seed];
@@ -287,12 +322,12 @@ Expr.prototype = {
 		var currentRule = this.rules["R"+seed];
 		var exprLenght = 0;
 		while (currentRule.indexOf("EXPR") > 0) {
-			// console.debug("Current Rule", currentRule);		
+			// console.debug("Current Rule", currentRule);
 
-			/* Split the expression and expand the EXPR 
+			/* Split the expression and expand the EXPR
 			var splited = currentRule.split(" ");
 
-			
+
 
 			seed = this.randomize(this.rulesAmount)
 			currentRule = this.rules["R"+seed];
@@ -308,14 +343,14 @@ Exp: (Numb Op Exp) Op Exp | [Numb Op Exp] Op Exp | {Numb Op Exp} Op Exp | Numb O
 
 
 
-Numb: [0-9] 
+Numb: [0-9]
 Op: [*-+/]
-R3: (Numb Op Exp) Op Exp 
-R4: [Numb Op Exp] Op Exp 
-R5: {Numb Op Exp} Op Exp 
-R6: Numb Op Exp 
+R3: (Numb Op Exp) Op Exp
+R4: [Numb Op Exp] Op Exp
+R5: {Numb Op Exp} Op Exp
+R6: Numb Op Exp
 R7: Numb
-R8: (Numb Op Exp) 
+R8: (Numb Op Exp)
 R9: [Numb Op Exp]
 R10: {Numb Op Exp}
 
@@ -327,7 +362,7 @@ R10: {Numb Op Exp}
 12 + 4 - (7 + {2 + Exp})		--> R10
 12 + 4 - (7 + {2 + 5})			--> R7
 
-		
+
 		R1: "( NUMB OP EXPR ) OP EXPR",
 		R2: "[ NUMB OP EXPR ] OP EXPR",
 		R3: "{ NUMB OP EXPR } OP EXPR",
